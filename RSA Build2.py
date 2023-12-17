9ce37c2db320dc9d441ab5601262d2661f8b3592
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2023-12-17 21:07:22 +0530

    Create RSA Build2.py

---
+++ RSA Build2.py
@@ -0,0 +1,60 @@
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+
+def generate_key_pair():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+
+    # Serialize keys to PEM format (you can also use other formats)
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    return private_pem, public_pem
+
+def encrypt(message, public_key):
+    public_key = serialization.load_pem_public_key(public_key, backend=default_backend())
+    ciphertext = public_key.encrypt(
+        message.encode('utf-8'),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return ciphertext
+
+def decrypt(ciphertext, private_key):
+    private_key = serialization.load_pem_private_key(private_key, password=None, backend=default_backend())
+    plaintext = private_key.decrypt(
+        ciphertext,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return plaintext.decode('utf-8')
+
+# Example usage:
+private_key, public_key = generate_key_pair()
+
+message_to_encrypt = "Hello everyone!"
+ciphertext = encrypt(message_to_encrypt, public_key)
+print("Encrypted:", ciphertext)
+
+decrypted_message = decrypt(ciphertext, private_key)
+print("Decrypted:", decrypted_message)
